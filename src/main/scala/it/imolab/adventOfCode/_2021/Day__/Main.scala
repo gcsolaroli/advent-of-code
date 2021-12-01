091d99f2b5e141085790854aed8da36e217af755
Giulio Cesare Solaroli <giuliocesare.solaroli@curaenergy.it>
2021-12-01 07:39:42 +0100

    Year 2021 - Day 01

---
+++ src/main/scala/it/imolab/adventOfCode/_2021/Day__/Main.scala
@@ -0,0 +1,23 @@
+package it.imolab.adventOfCode._2021.Day__
+
+val puzzle = ???
+
+@main def answer_1 =
+/*
+
+*/
+  val result = ???
+
+  println(s"Day __ - answer 1: $result")
+end answer_1
+
+// ===================================================================
+
+@main def answer_2 =
+/*
+
+*/
+  val result = ???
+
+  println(s"Day __ - answer 2: $result")
+end answer_2
